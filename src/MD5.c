13b4509c5dc0e9fd0775286ee4d3903d801db2ef
arian-deimling <arianm@deimling.net>
2021-03-28 20:29:48 -0400

    Modified MD5 hash function to handle any length string; also added inplace_ prefix to the function name

--- src/MD5.c
+++ src/MD5.c
@@ -4,7 +4,9 @@ uint32_t left_rotate(uint32_t x, uint8_t bits) {
   return (x << bits) | (x >> (32 - bits));
 }
 
-void md5_sum(uint8_t* string, uint8_t string_len, uint8_t* hash_result) {
+void inplace_md5_sum(uint8_t* string,
+                     const uint32_t string_length,
+                     uint8_t* hash_result) {
 
   // see https://en.wikipedia.org/wiki/MD5#Pseudocode for MD5 implementation
   // information this implementation only handles hashes of strings of up to
@@ -40,76 +42,99 @@ void md5_sum(uint8_t* string, uint8_t string_len, uint8_t* hash_result) {
     0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
   };
 
-  // used to initialize the values A, B, C, and D - normally change after each
-  // 512-bit chunk is processed in a typical MD5 implementation, but never
-  // change in this implementation because we are only hashing strings that are
-  // processed into a maximum of 1 512-bit chunk
-  static const uint32_t A0 = 0x67452301;
-  static const uint32_t B0 = 0xefcdab89;
-  static const uint32_t C0 = 0x98badcfe;
-  static const uint32_t D0 = 0x10325476;
+  // use these values to initialize A, B, C, and D; after the values are
+  // manipulated for a given 512-bit chunk, add A, B, C, D back into their 
+  // respective values below
+  uint32_t A0 = 0x67452301;
+  uint32_t B0 = 0xefcdab89;
+  uint32_t C0 = 0x98badcfe;
+  uint32_t D0 = 0x10325476;
 
-  // padding must be added to end of string to make it's length 64 bytes - add
-  // "0b10000000" byte followed by "0b00000000" bytes until length is 64 bytes
-  string[string_len] = 0x80;
-  memset(&string[string_len + 1], 0, 64 - (string_len + 1 + 1));
+  // MD5 padding is specified as being a singular 1 bit followed by 0 bits
+  // until we are 8 bytes away from the end of a 512-bit chunk; the length
+  // of the message in bits is added as an unsigned int in the last 8 bytes
+  //
+  // we will add a whole 0x80 byte for the 1 bit padding, then we must also
+  // have enough room to add the 8-byte unsigned int; therefore, add 9
+  // to the string length, then perform the ceiling of that number divided
+  // by 64 (bytes) to get the number of 512-bit chunks that we will need
+  uint32_t chunks = ((string_length + 1 + 8) / 64)  // int division portion
+    + ((string_length + 1 + 8) % 64 != 0);          // ceiling portion
 
-  // reinterpret the last 64 bits of the padding as a unsigned int and set its
+  // add 1 bit padding followed by the appropriate amount of 0 bit padding
+  string[string_length] = 0x80;
+
+  // after accounting for the string and the addition byte added above, add
+  // enough padding to fill out to the end of the final 512-bit chunk
+  memset(&string[string_length + 1], 0, chunks * 64 - (string_length + 1));
+
+  // reinterpret the last 8 bytes of the padding as a unsigned int and set its
   // value to length of the message in bits (little endian)
-  *((uint64_t*) &string[56]) = string_len * 8;
+  *((uint64_t*) &string[64 * chunks - 8]) = string_length * 8;
 
-  // reinterpret the 512-bit padded message as 16 32-bit unsigned integers
-  uint32_t* M = (uint32_t*) string;
+  // iterate over each chunk in the padded message
+  for (int chunk_number = 0; chunk_number < chunks; chunk_number++) {
 
-  // initialize the values of A, B, C, D
-  uint32_t A = A0;
-  uint32_t B = B0;
-  uint32_t C = C0;
-  uint32_t D = D0;
+    // reinterpret the current 512-bit chunk as 16 32-bit unsigned integers
+    uint32_t* M = (uint32_t*) &string[chunk_number * 64];
+ 
+    // initialize the values of A, B, C, D
+    uint32_t A = A0;
+    uint32_t B = B0;
+    uint32_t C = C0;
+    uint32_t D = D0;
+
+    // there are 64 rounds per 512-bit block in the MD5 hashing algorithm
+    for (int i = 0; i < 64; i++) {
+
+      // value F and index g are computer and used to modify the value of B
+      uint32_t F, g;
+
+      // for rounds 0 - 15, this is the calculation for F and g
+      if (i < 16) {
+        F = (B & C) | ((~B) & D);
+        g = i;
+
+      // for rounds 16 - 31, this is the calculation for F and g
+      } else if (i < 32) {
+        F = (D & B) | ((~D) & C);
+        g = (5 * i + 1) % 16;
+
+      // for rounds 32 - 47, this is the calculation for F and g
+      } else if (i < 48) {
+        F = B ^ C ^ D;
+        g = (3 * i + 5) % 16;
+
+      // for rounds 48 - 63, this is the calculation for F and g
+      } else {
+        F = C ^ (B | (~D));
+        g = (7 * i) % 16;
+
+      }
+
+      // for each round, use the following equations to compute the new values
+      // of A, B, C, D
+      F = F + A + K[i] + M[g];
+      A = D;
+      D = C;
+      C = B;
+      B = B + left_rotate(F, s[i]);
   
-  // there are 64 rounds per 512-bit block in the MD5 hashing algorithm
-  for (int i = 0; i < 64; i++) {
-
-    // value F and index g are computer and used to modify the value of B
-    uint32_t F, g;
-
-    // for rounds 0 - 15, this is the calculation for F and g
-    if (i < 16) {
-      F = (B & C) | ((~B) & D);
-      g = i;
-
-    // for rounds 16 - 31, this is the calculation for F and g
-    } else if (i < 32) {
-      F = (D & B) | ((~D) & C);
-      g = (5 * i + 1) % 16;
-
-    // for rounds 32 - 47, this is the calculation for F and g
-    } else if (i < 48) {
-      F = B ^ C ^ D;
-      g = (3 * i + 5) % 16;
-
-    // for rounds 48 - 63, this is the calculation for F and g
-    } else {
-      F = C ^ (B | (~D));
-      g = (7 * i) % 16;
-
     }
 
-    // for each round, use the following equations to compute the new values
-    // of A, B, C, D
-    F = F + A + K[i] + M[g];
-    A = D;
-    D = C;
-    C = B;
-    B = B + left_rotate(F, s[i]);
+    // add the values of A, B, C, D into A0, B0, C0, D0 after each round
+    A0 += A;
+    B0 += B;
+    C0 += C;
+    D0 += D;
 
   }
 
-  // compute and output to the hash-result array, the hash value
-  ((uint32_t*) hash_result)[0] = A + A0;
-  ((uint32_t*) hash_result)[1] = B + B0;
-  ((uint32_t*) hash_result)[2] = C + C0;
-  ((uint32_t*) hash_result)[3] = D + D0;
+  // use A0, B0, C0, D0 to set the values of the output hash result
+  ((uint32_t*) hash_result)[0] = A0;
+  ((uint32_t*) hash_result)[1] = B0;
+  ((uint32_t*) hash_result)[2] = C0;
+  ((uint32_t*) hash_result)[3] = D0;
 
   return;
 
